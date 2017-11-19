bb60caf1508f79153fac83419bc315402096f519
rolkra <31808268+rolkra@users.noreply.github.com>
2017-11-19 12:56:24 +0100

    Update learn_model.R

--- learn_model.R
+++ learn_model.R
@@ -7,5 +7,12 @@ df <- iris
 x <- df$Sepal.Length
 y <- df$Petal.Length
 plot (x,y)
-l <- lm(y~x)
-abline(l, col="red",lty="dotted",lwd=2)
+mod <- lm(y~x)
+abline(mod, col="red",lty="dotted",lwd=2)
+
+coef(mod)         # shows intercept (y at x=0) and slope (dy/dx) 
+residuals(mod)    # shows the residuals (predicted value - real value)
+predict(mod)      # predicts 
+
+library(broom)
+augment(mod)      # creates a dataframe with real values, predicted values, residuals, ...
