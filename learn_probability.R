4c7daca4a7474486fa064372bfb511a1d329064f
rolkra <31808268+rolkra@users.noreply.github.com>
2017-11-26 16:13:57 +0100

    Update learn_probability.R

--- learn_probability.R
+++ learn_probability.R
@@ -1,8 +1,6 @@
 #############################################################################
-## probability
-#############################################################################
-
 ## uniform distirbution
+#############################################################################
 
 # 10 random numbers between 0 and 1
 runif(10)
@@ -10,7 +8,10 @@ runif(10)
 # 10 random numbers between 1 and 10
 runif(n = 10, min = 1, max = 10)
 
+
+#############################################################################
 ## binomial distribution
+#############################################################################
 
 # set seed to get reproducable result
 set.seed(100)                              
@@ -38,7 +39,9 @@ pbinom(10, 60, 1/6) - pbinom(7, 60, 1/6)
 dbinom(8, 60, 1/6) + dbinom(9, 60, 1/6) + dbinom(10, 60, 1/6)
 
 
+#############################################################################
 ## normal distribution
+#############################################################################
 
 # set seed to get reproducable result
 set.seed(100)                              
@@ -46,3 +49,27 @@ x <- seq(0,25, by=0.25)
 y <- dnorm(x, mean = 10, sd = 4)
 plot(x,y, main = "normal distribution")
 lines(x,y)
+
+
+#############################################################################
+## hypergeometric distribution (picking black/white balls from an urn)
+#############################################################################
+
+# nn = numer of experiments
+# m  = number of white balls in urn 
+# n  = number of black balls in urn
+# k  = number of balls taken out
+
+# urn with 9 white and 1 black balls, 1 ball taken out, experiment 100x
+# number of white balls per experiment
+white <- rhyper(nn = 100, m=9, n=1, k=1)     
+
+# urn with 5 white and 5 black balls, 5 balls taken out, experiment 10000x
+# number of white balls per experiment
+white <- rhyper(nn = 10000, m=5, n=5, k=5)     
+plot(table(white), xlim = c(0,5))
+
+# denisity
+x <- c(1:5)
+y <- dhyper(x = x, m=5, n=5, k=5) 
+barplot(y, names.arg = x, main = "probability (hypergeomentric)\npicking n white balls out of 5 white and 5 black")
